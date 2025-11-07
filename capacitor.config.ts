e31ec2056788a0e210111c3c93a33ed2c951fdf9
dell dell <dell25252025@gmail.com>
2025-11-07 20:18:00 +0000

    Fix: Corrige la configuration de Capacitor.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,9 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.wanderlink.app',
   appName: 'WanderLink',
-  webDir: 'out',
+  webDir: '.next',
   server: {
-    url: 'http://192.168.100.26:3000', 
-    cleartext: true
+    androidScheme: 'https'
   }
 };
 
